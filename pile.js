fe98b7c5108812abe5ba517676ee5f11e2a5766f
Gabriel Farrell <g@grrawr.com>
2012-11-03 02:51:57 -0400

    Pass err on to next

--- pile.js
+++ pile.js
@@ -1,11 +1,12 @@
 module.exports = function pile(/*layers*/) {
   var args = Array.prototype.slice.call(arguments)
   var last = args.pop()
-  return function(req, res, next) {
+  return function (req, res, next) {
     var pending = args.length
     if (!pending) return last(req, res, next)
-    args.forEach(function(layer) {
-      layer(req, res, function(err) {
+    args.forEach(function (layer) {
+      layer(req, res, function (err) {
+        if (err) return next(err)
         if (!--pending) last(req, res, next)
       }) 
     })
